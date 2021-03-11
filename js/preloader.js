33eb6784bba128cf9560b332fb401c8047e6524e
Kevin <kevinbulfanisay1@gmail.com>
2021-03-11 11:49:54 +0300

    first commit

--- js/preloader.js
+++ js/preloader.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /* Theme: Niwax - Creative Agency & Portfolio HTML Template
 Author: Rajesh-Doot	
 File Description: preloader js */
@@ -10,17 +9,4 @@ File Description: preloader js */
         preloadpage.style.display = "none";
   });
 
-=======
-/* Theme: Niwax - Creative Agency & Portfolio HTML Template
-Author: Rajesh-Doot	
-File Description: preloader js */
-(function ($) {
-  "use strict";
-  //Preloader
-  window.addEventListener( 'load', function () {
-        var preloadpage = document.getElementById( "page_loader" );
-        preloadpage.style.display = "none";
-  });
-
->>>>>>> 0e338ba991fdd4d44816790764aea1983cbd9192
 })(jQuery);
